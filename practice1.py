189fd7dc51f9a563a8076f5f6089e796b518f6d8
elofongo <elizabeth@edpassare.com>
2025-08-07 16:39:13 -0500

    These are 3 turtle functions. The first makes a donut shape, the second makes a square, and the third makes a triangle.

---
+++ practice1.py
@@ -0,0 +1,35 @@
+from turtle import *
+
+def donut():
+    circle(30)
+    left(90)
+    left(180)
+    color('white')
+    forward(60)
+    color('black')
+    left(90)
+    circle(90)
+
+donut()
+
+def quad():
+    forward(100)
+    left(90)
+    forward(100)
+    left(90)
+    forward(100)
+    left(90)
+    forward(100)
+    left(90)
+
+quad()
+
+def threeshape():
+    forward(100)
+    left(120)
+    forward(100)
+    left(120)
+    forward(100)
+    left(120)
+
+threeshape()
